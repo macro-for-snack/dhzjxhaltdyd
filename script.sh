36722095746692fca86ab133c91a94a57301ddc0
macro-for-snack <84444763+macro-for-snack@users.noreply.github.com>
2021-05-19 21:16:35 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,17 +5,18 @@ git init -q
 
 RANDOM=`date "+%N"`
 min_val=1
-max_val=5
+max_val=70
 mod_val=`echo "$max_val - $min_val + 1" | bc`
 random_val=`echo "($RANDOM % $mod_val) + $min_val" | bc`
 echo $random_val
+
+git config user.name $USERNAME
+git config user.email $USERMAIL
 
 for ((i=0;i<$random_val;i++))
 do
   echo $(TZ=":Asia/Seoul" date '+%Y--%m--%d_%H:%M:%S') >> commit.txt
   git add .
   git commit -m $(TZ=":Asia/Seoul" date '+%Y--%m--%d_%H:%M:%S')
-  git config user.name $USERNAME
-  git config user.email $USERMAIL
   git push -u origin master
 done
