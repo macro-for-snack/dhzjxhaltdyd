97a57676ec8ae7e8b5297103264f76f176f8f2c1
macro-for-snack <84444763+macro-for-snack@users.noreply.github.com>
2021-05-19 21:06:41 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,7 @@ max_val=5
 mod_val=`echo "$max_val - $min_val + 1" | bc`
 random_val=`echo "($RANDOM % $mod_val) + $min_val" | bc`
 
-for (i=0; i< $random_val ;i++)
+for ((i=0;i<$random_val;i++))
 do
   echo $(TZ=":Asia/Seoul" date '+%Y--%m--%d_%H:%M:%S') >> commit.txt
   git add .
