08e00b8b95bb69cd99d401e66cdd004d4aa9de30
macro-for-snack <84444763+macro-for-snack@users.noreply.github.com>
2022-04-19 11:06:29 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,6 +13,10 @@ echo $random_val
 git config user.name $USERNAME
 git config user.email $USERMAIL
 
+if [[ $(TZ=":Asia/Seoul" date +%u) -gt 5 ]]; then
+    random_val=`echo "($RANDOM % 2) + $min_val" | bc`
+fi
+
 for ((i=0;i<$random_val;i++))
 do
   echo $(TZ=":Asia/Seoul" date '+%Y--%m--%d_%H:%M:%S') >> commit.txt
